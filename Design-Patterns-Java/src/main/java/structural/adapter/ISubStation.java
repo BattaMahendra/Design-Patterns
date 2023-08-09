1b12194c503f86059a624015d84e9b1916e30649
BattaMahendra <bmc26g@gmail.com>
2023-08-09 12:10:18 +0530

    adapter pattern

--- Design-Patterns-Java/src/main/java/structural/adapter/ISubStation.java
+++ Design-Patterns-Java/src/main/java/structural/adapter/ISubStation.java
@@ -1,5 +1,10 @@
 package structural.adapter;
 
+/*
+ * lets suppose this is the main interface which we want to use.
+ * this interface has one method which returns the power based on the client
+ * the implementation of this interface returns 11k volts by default
+ */
 public interface ISubStation {
 	
 	public Power getPower(String client);
--- Design-Patterns-Java/src/main/java/structural/adapter/package-info.java
+++ Design-Patterns-Java/src/main/java/structural/adapter/package-info.java
@@ -4,3 +4,14 @@ package structural.adapter;
 * and its used so that two unrelated interfaces can work together. 
 *The object that joins these unrelated interface is called an Adapter.
 */
+
+/**
+ * Here we have a ISubstation interface and ITransformer interface.
+ * generally substation interface provides the power to the client. But the catch is it porvides 
+ * default 11k power to any client regardless
+ * But our client needs the variable power so client extra transformer functionality
+ * 
+ * So now we use the adapter to combine the both interfaces so that the client can extend the same 
+ * Substation interface but he also can get the required lower amounts of power from transformer.
+ * 
+ */
