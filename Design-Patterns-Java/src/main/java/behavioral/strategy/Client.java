f0e05d400480cc5ff73fa77dc7d90a075d4669d6
BattaMahendra <bmc26g@gmail.com>
2024-02-19 21:19:41 +0530

    modified client class

--- Design-Patterns-Java/src/main/java/behavioral/strategy/Client.java
+++ Design-Patterns-Java/src/main/java/behavioral/strategy/Client.java
@@ -5,17 +5,41 @@ public class Client {
 
     public static void main(String[] args) {
 
-        CheckInWithoutStrategy c = new CheckInWithoutStrategy();
-        c.setCheckInType(ACCESS_CARD_CHECK_IN);
-        c.checkIn("creds");
+    	System.out.println("\n Without using strategy pattern \n");
+        withOutStrategy();
         
-        AccessCard accessCard = new AccessCard();
-        Context context = new Context(accessCard);
-        context.checkIn("creds");
+        System.out.println("\n\n With using strategy pattern \n");
+        withStrategyPattern();
         
-        Context context1 = new Context(Context.accCard);
-        context1.checkIn("creds");
+        System.out.println("\n Without using strategy pattern and JAVA 8 \n");
+        withStrategyAndJava8();
         	
         
     }
+
+	private static void withStrategyAndJava8() {
+		Context context1 = new Context(Context.accCard);
+        context1.checkIn("creds");
+        
+        Context context2 = new Context(Context.wfh);
+        context1.checkIn("creds");
+        
+	}
+
+	private static void withStrategyPattern() {
+		AccessCard accessCard = new AccessCard();
+        Context context = new Context(accessCard);
+        context.checkIn("creds");
+        
+        LanCheckIn lan = new LanCheckIn();
+        Context context2 = new Context(lan);
+        context2.checkIn("creds");
+	}
+
+	private static void withOutStrategy() {
+		CheckInWithoutStrategy c = new CheckInWithoutStrategy();
+        c.setCheckInType(ACCESS_CARD_CHECK_IN);
+        c.checkIn("creds");
+        
+	}
 }
